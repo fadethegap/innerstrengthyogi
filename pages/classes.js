c0ef8d9287779ad10e9fb7a2554cf6937079e9d2
John Kovach <fadethegap@hotmail.com>
2022-09-19 12:03:43 -0400

    Hard coded images and video

--- pages/classes.js
+++ pages/classes.js
@@ -4,65 +4,65 @@ import Link from "next/link";
 import Image from "next/image";
 import Moment from "react-moment";
 
-// import Yoga1 from "../public/yoga1.jpg";
-// import Yoga2 from "../public/yoga2.jpg";
-// import Yoga3 from "../public/yoga5.jpg";
+import image1 from "../public/smokeSpiral.jpg";
+import image2 from "../public/greenFibonacci.jpg";
+import image3 from "../public/shell1.jpg";
 
-// const classes = [
-//   {
-//     title: "Play Together, Stay Together",
-//     href: "#",
-//     category: { name: "Article", href: "#" },
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
-//     date: "Mar 16, 2020",
-//     datetime: "2020-03-16",
-//     imageUrl: Yoga1,
-//     readingTime: "6 min",
-//     author: {
-//       name: "Roel Aufderehar",
-//       href: "#",
-//       imageUrl:
-//         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-//     },
-//   },
-//   {
-//     title: "Health and Vitality With Friends",
-//     href: "#",
-//     category: { name: "Video", href: "#" },
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
-//     date: "Mar 10, 2020",
-//     datetime: "2020-03-10",
-//     imageUrl: Yoga2,
-//     readingTime: "4 min",
-//     author: {
-//       name: "Brenna Goyette",
-//       href: "#",
-//       imageUrl:
-//         "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-//     },
-//   },
-//   {
-//     title: "Family Focused Fun",
-//     href: "#",
-//     category: { name: "Case Study", href: "#" },
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
-//     date: "Feb 12, 2020",
-//     datetime: "2020-02-12",
-//     imageUrl: Yoga3,
-//     readingTime: "11 min",
-//     author: {
-//       name: "Daniela Metz",
-//       href: "#",
-//       imageUrl:
-//         "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-//     },
-//   },
-// ];
+const classes = [
+  {
+    title: "Play Together, Stay Together",
+    href: "#",
+    category: { name: "Article", href: "#" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    imageUrl: image1,
+    readingTime: "6 min",
+    author: {
+      name: "Roel Aufderehar",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: "Health and Vitality With Friends",
+    href: "#",
+    category: { name: "Video", href: "#" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
+    date: "Mar 10, 2020",
+    datetime: "2020-03-10",
+    imageUrl: image2,
+    readingTime: "4 min",
+    author: {
+      name: "Brenna Goyette",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: "Workshop",
+    href: "#",
+    category: { name: "Case Study", href: "#" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: image3,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+];
 
-export default function Classes({ classes }) {
+export default function Classes() {
   const [imageURL, setImageURL] = useState(null);
 
   // if (data?.image_filename) {
@@ -100,7 +100,7 @@ export default function Classes({ classes }) {
                   <Moment format="MM/DD/YYYY">{c.created_at}</Moment>
                 </div> */}
                 <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
-                  {/* <div className="flex-shrink-0 relative">
+                  <div className="flex-shrink-0 relative">
                     <Image
                       className="h-48 w-full object-cover"
                       src={c.imageUrl}
@@ -108,7 +108,7 @@ export default function Classes({ classes }) {
                       // layout="fill"
                       //   objectFit="contain"
                     />
-                  </div> */}
+                  </div>
                   <div className="flex flex-1 flex-col justify-between bg-white p-6">
                     <div className="flex-1">
                       <a href={c.href} className="mt-2 block">
@@ -131,12 +131,12 @@ export default function Classes({ classes }) {
   );
 }
 
-export const getStaticProps = async () => {
-  const { data: classes } = await supabase.from("classes").select("*");
+// export const getStaticProps = async () => {
+//   const { data: classes } = await supabase.from("classes").select("*");
 
-  return {
-    props: {
-      classes,
-    },
-  };
-};
+//   return {
+//     props: {
+//       classes,
+//     },
+//   };
+// };

3e915caf0a80b93f620ff24287af7673f5301612
John Kovach <fadethegap@hotmail.com>
2022-10-05 12:06:26 -0400

    Updated address

--- pages/location.js
+++ pages/location.js
@@ -11,9 +11,20 @@ export default function Location() {
             <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900">
               How to Find Us
             </h2>
+
+            {/* <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3098.0143587090356!2d-76.57502148426424!3d39.06058694491259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7fa40630b1fcb%3A0xca3ca2e7f77aa6f2!2s389%20Hemlock%20Trail%2C%20Crownsville%2C%20MD%2021032!5e0!3m2!1sen!2sus!4v1664985283703!5m2!1sen!2sus"
+              width="600"
+              height="450"
+              style="border:0;"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe> */}
+
             <div className="mt-12 flex justify-center h-[450px]">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4551.541353236336!2d-76.5741133577061!3d39.060037310465425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7fa406f812743%3A0x3eed4d15d78e6e2d!2s390%20Locust%20Trail%2C%20Crownsville%2C%20MD%2021032!5e0!3m2!1sen!2sus!4v1664028075847!5m2!1sen!2sus"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3098.0143587090356!2d-76.57502148426424!3d39.06058694491259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7fa40630b1fcb%3A0xca3ca2e7f77aa6f2!2s389%20Hemlock%20Trail%2C%20Crownsville%2C%20MD%2021032!5e0!3m2!1sen!2sus!4v1664985283703!5m2!1sen!2sus"
                 // width="600"
                 // height="450"
                 width="100%"
@@ -26,7 +37,7 @@ export default function Location() {
             </div>
             <div className="mt-5">
               <p className="underline">Address</p>
-              <p>390 Locust Trail</p>
+              <p>389 Hemlock Trail</p>
               <p>Crownsville, MD 21032</p>
             </div>
             <div className="mt-5">
